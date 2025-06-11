579a40ab3fd9bc6042eda88d5b2c50e7b528ef53
RyeinKim <ryein6896@gmail.com>
2025-06-11 11:37:21 +0900

    ConfigMap 코드

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-
-console.log("[MAIN] Express app is starting...");
-console.log("[MAIN] App is now running");
+const message = process.env.MESSAGE || "Hello from default!";
 
 app.get("/", (req, res) => {
-    res.send("Hello from lifecycle demo!");
+    res.send(message);
 });
 
-setTimeout(() => {
-    console.log("[MAIN] Simulating graceful shutdown in 5s...");
-    process.exit(0);
-}, 5000); // 자동 종료: preStop 확인을 위한 시뮬레이션
-
 app.listen(port, () => {
-    console.log(`[MAIN] App listening on port ${port}`);
+    console.log(`App running on port ${port} with message: ${message}`);
 });
