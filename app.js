f79023f91b65f3751559898d6c32b409c035d9c2
RyeinKim <ryein6896@gmail.com>
2025-06-12 01:45:11 +0900

    Volume 코드

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express");
+const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
 
-const siteName = process.env.SITE_NAME || "default-site";
-const adminPassword = process.env.ADMIN_PASSWORD || "not-set";
-
 app.get("/", (req, res) => {
-    res.send(`Site: ${siteName}, Admin PW: ${adminPassword}`);
+    try {
+        const content = fs.readFileSync("/config/message.txt", "utf8");
+        res.send(`File message: ${content}`);
+    } catch (e) {
+        res.send("No config file found.");
+    }
 });
 
 app.listen(port, () => {
-    console.log(`[APP] Running on ${port}, SITE_NAME=${siteName}, ADMIN_PASSWORD=${adminPassword}`);
+    console.log(`[APP] Running on port ${port}`);
 });
