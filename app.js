01a3cace9ee4f75e278899831b525f0a48cb0f47
RyeinKim <ryein6896@gmail.com>
2025-06-12 12:12:10 +0900

    Networking 코드

--- app.js
+++ app.js
@@ -1,28 +1,11 @@
 const express = require("express");
-const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
-const path = "/data/record.txt";
 
-app.use(express.json());
-
-// 쓰기
-app.post("/", (req, res) => {
-    const msg = req.body.message || "empty";
-    fs.writeFileSync(path, msg, "utf8");
-    res.send("Message saved.");
-});
-
-// 읽기
 app.get("/", (req, res) => {
-    if (fs.existsSync(path)) {
-        const content = fs.readFileSync(path, "utf8");
-        res.send(`Saved message: ${content}`);
-    } else {
-        res.send("No message found.");
-    }
+    res.send(`Hello from ${process.env.NAME}`);
 });
 
 app.listen(port, () => {
-    console.log(`[APP] Server running on ${port}`);
+    console.log(`[APP] ${process.env.NAME} running on port ${port}`);
 });
