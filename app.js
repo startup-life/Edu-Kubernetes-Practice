3e43a7978504eb629b8e3a69f321ef2785202cfd
RyeinKim <ryein6896@gmail.com>
2025-06-12 11:19:31 +0900

    PV/PVC 코드

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 const port = process.env.PORT || 3000;
+const path = "/data/record.txt";
 
+app.use(express.json());
+
+// 쓰기
+app.post("/", (req, res) => {
+    const msg = req.body.message || "empty";
+    fs.writeFileSync(path, msg, "utf8");
+    res.send("Message saved.");
+});
+
+// 읽기
 app.get("/", (req, res) => {
-    try {
-        const content = fs.readFileSync("/config/message.txt", "utf8");
-        res.send(`File message: ${content}`);
-    } catch (e) {
-        res.send("No config file found.");
+    if (fs.existsSync(path)) {
+        const content = fs.readFileSync(path, "utf8");
+        res.send(`Saved message: ${content}`);
+    } else {
+        res.send("No message found.");
     }
 });
 
 app.listen(port, () => {
-    console.log(`[APP] Running on port ${port}`);
+    console.log(`[APP] Server running on ${port}`);
 });
