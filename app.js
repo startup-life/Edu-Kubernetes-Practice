d6dd363969fb9cf051470793437094b3a354c82c
RyeinKim <ryein6896@gmail.com>
2025-06-13 15:36:53 +0900

    VPA 코드

--- app.js
+++ app.js
@@ -2,23 +2,14 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
-let load = false;
+setInterval(() => {
+    for (let i = 0; i < 1e7; i++) Math.sqrt(i);
+}, 100);
 
 app.get("/", (req, res) => {
-    res.send("Hello from HPA target\n");
+    res.setHeader("Content-Type", "text/plain");
+    res.send("Hello from VPA target\n");
 });
-
-app.get("/load", (req, res) => {
-    load = true;
-    res.send("CPU load started\n");
-});
-
-// 백그라운드 CPU 부하
-setInterval(() => {
-    if (load) {
-        for (let i = 0; i < 1e8; i++) Math.sqrt(i);
-    }
-}, 100);
 
 app.listen(port, () => {
     console.log(`[APP] running on port ${port}`);
