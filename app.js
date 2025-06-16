37f29e7fabf0cff3ca2801733bce492fa088744b
RyeinKim <ryein6896@gmail.com>
2025-06-16 16:13:27 +0900

    Push v2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (_, res) => {
-    res.send('Hello from Github Actions + ArgoCD CI/CD!');
+    res.send('Hello from Github Actions + ArgoCD CI/CD! - This is Version 2');
 });
 
 app.listen(port, () => {
