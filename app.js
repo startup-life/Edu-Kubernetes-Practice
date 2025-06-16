08009e238120e9487f4893bfe2fd14502d7898d9
RyeinKim <ryein6896@gmail.com>
2025-06-16 16:07:08 +0900

    ArgoCD with github actions

--- app.js
+++ app.js
@@ -1,26 +1,11 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
-const path = "/data/message.txt";
 
-app.use(express.json());
-
-app.post("/", (req, res) => {
-    const msg = req.body.message || "empty";
-    fs.writeFileSync(path, msg, "utf8");
-    res.send("Message saved.");
-});
-
-app.get("/", (req, res) => {
-    if (fs.existsSync(path)) {
-        const content = fs.readFileSync(path, "utf8");
-        res.send(`Saved message: ${content}`);
-    } else {
-        res.send("No message found.");
-    }
+app.get('/', (_, res) => {
+    res.send('Hello from Github Actions + ArgoCD CI/CD!');
 });
 
 app.listen(port, () => {
-    console.log(`[APP] running on port ${port}`);
+    console.log(`App listening on port ${port}`);
 });
